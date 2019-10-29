267936acf906a25a158f2a56891759d7e2a6afbf
Pangjha <poppingloveyou@gmail.com>
2019-10-30 01:47:01 +0700

    finished 5.4

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
@@ -78,6 +80,10 @@ class GameViewModel : ViewModel() {
         val currentTime: LiveData<Long>
             get() = _currentTime
         private val timer: CountDownTimer
+        // The String version of the current time
+        val currentTimeString = Transformations.map(currentTime) { time ->
+            DateUtils.formatElapsedTime(time)
+        }
 
     }
     init {
