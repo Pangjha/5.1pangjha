0586d301e143b847d4de67842ef5fbe44cf0d9f0
Pangjha <poppingloveyou@gmail.com>
2019-10-29 23:49:32 +0700

    encape livedata

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -9,10 +10,15 @@ import androidx.lifecycle.ViewModel
  */
 class GameViewModel : ViewModel() {
 
-    // The current word
-    val word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
+
     // The current score
-    val score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
+
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -52,8 +58,9 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+
+        _score.value = 0
     }
 
     /**
@@ -67,13 +74,13 @@ class GameViewModel : ViewModel() {
     /** Methods for updating the UI **/
     fun onSkip() {
         if (!wordList.isEmpty()) {
-            score.value = (score.value)?.minus(1)
+            _score.value = (_score.value)?.minus(1)
         }
         nextWord()
     }
     fun onCorrect() {
         if (!wordList.isEmpty()) {
-            score.value = (score.value)?.plus(1)
+            _score.value = (_score.value)?.plus(1)
         }
         nextWord()
     }
@@ -84,7 +91,7 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 }
